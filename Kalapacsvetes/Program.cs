c9b18e3de8a3d96587a2f93f472a83490991508c
Dallos Miklós <93658026+dallosmiklos@users.noreply.github.com>
2024-12-15 13:58:42 +0100

    a, b és c feladatok elkészítése

---
+++ Kalapacsvetes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Kalapacsvetes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Sportolo> sportolok = new List<Sportolo>();
+
+                string[] sorok = File.ReadAllLines("kalapacsvetes.txt");
+
+            // Első sor kihagyása (fejléc)
+            for (int i = 1; i < sorok.Length; i++)
+            {
+                string[] adatok = sorok[i].Split(';');
+
+                int helyezes = int.Parse(adatok[0]);
+                double eredmeny = double.Parse(adatok[1].Replace(',', '.'), CultureInfo.InvariantCulture);
+                string nev = adatok[2];
+                string orszagKod = adatok[3];
+                string helyszin = adatok[4];
+                DateTime datum = DateTime.Parse(adatok[5]);
+
+                sportolok.Add(new Sportolo(helyezes, eredmeny, nev, orszagKod, helyszin, datum));
+            }
+
+            foreach (var sportolo in sportolok)
+            {
+                Console.WriteLine(sportolo);
+            }
+        }
+    }
+}
---
+++ Kalapacsvetes/Sportolo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalapacsvetes
+{
+    internal class Sportolo
+    {
+        public int Helyezes { get; set; }
+        public double Eredmeny { get; set; }
+        public string Nev { get; set; }
+        public string OrszagKod { get; set; }
+        public string Helyszin { get; set; }
+        public DateTime Datum { get; set; }
+
+        public Sportolo(int helyezes, double eredmeny, string nev, string orszagKod, string helyszin, DateTime datum)
+        {
+            Helyezes = helyezes;
+            Eredmeny = eredmeny;
+            Nev = nev;
+            OrszagKod = orszagKod;
+            Helyszin = helyszin;
+            Datum = datum;
+        }
+
+        public override string ToString()
+        {
+            return $"{Helyezes}; {Eredmeny}m; {Nev}; {OrszagKod}; {Helyszin}; {Datum.ToShortDateString()}";
+        }
+    }
+
+}
