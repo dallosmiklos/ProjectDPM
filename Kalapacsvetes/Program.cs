d416b85e7b00b7777eefec5db43700fb1237b35f
SzlepakDominik <82323591+SzlepakDominik@users.noreply.github.com>
2024-12-17 21:04:34 +0100

    Add files via upload

--- Kalapacsvetes/Program.cs
+++ Kalapacsvetes/Program.cs
@@ -13,100 +13,97 @@ namespace Kalapacsvetes
     {
 
 
-        static void Befejez()
+        static void Befejez() //Enter lenyomásával bezárja a programot
         {
-            Console.WriteLine("Enterre befejez");
+            Console.WriteLine("Enter-re bezárja a programot");
             Console.ReadLine();
         }
 
         static void Main(string[] args)
         {
-            List<Sportolo> sportolok = new List<Sportolo>();
+            List<Sportolo> sportolok = new List<Sportolo>(); //Létrehoz egy listát a sportolókról
 
-                string[] sorok = File.ReadAllLines("kalapacsvetes.txt");
+            string[] sorok = File.ReadAllLines("kalapacsvetes.txt"); // Beolvassa a megadott file-t
 
             // Első sor kihagyása (fejléc)
             for (int i = 1; i < sorok.Length; i++)
             {
                 string[] adatok = sorok[i].Split(';');
 
-                int helyezes = int.Parse(adatok[0]);
-                double eredmeny = double.Parse(adatok[1].Replace(',', '.'), CultureInfo.InvariantCulture);
-                string nev = adatok[2];
-                string orszagKod = adatok[3];
-                string helyszin = adatok[4];
-                DateTime datum = DateTime.Parse(adatok[5]);
+                int helyezes = int.Parse(adatok[0]); //Kiirja az eredményeket számként
+                double eredmeny = double.Parse(adatok[1].Replace(',', '.'), CultureInfo.InvariantCulture); //Kiirja az eredményeket számként
+                string nev = adatok[2]; //Kiirja a versenyzők nevét
+                string orszagKod = adatok[3];//Kiirja hogy az ország kódját ahol a verseny volt tartva
+                string helyszin = adatok[4];//Kiirja a verseny helyszinét
+                DateTime datum = DateTime.Parse(adatok[5]);//Kiirja a verseny dátumát
 
                 sportolok.Add(new Sportolo(helyezes, eredmeny, nev, orszagKod, helyszin, datum));
             }
-
+            //Sportólók listájának kiirása
             foreach (var sportolo in sportolok)
             {
                 Console.WriteLine(sportolo);
             }
 
+                Console.WriteLine("\nStatisztikák");
+                Console.WriteLine($"Legjobb eredmény: {sportolok.Max(s => s.Eredmeny)} m");//Kiirja a Legtávolobb dobott eredményt Méterben
+                Console.WriteLine($"Legrosszabb eredmény: {sportolok.Min(s => s.Eredmeny)} m");//Kiirja a legrosszabb dobott eredményt méterben
+                Console.WriteLine($"Átlagos eredmény: {sportolok.Average(s => s.Eredmeny):F2} m");//Kiirja az átlagolt eredményt
+                Console.WriteLine($"Resztvevő versenyzők száma: {sportolok.Count}"); //Kiirja a résztvevőket összegezve
 
 
-
-
-            Console.WriteLine("\nStatisztikák");
-            Console.WriteLine($"Legjobb eredmény: {sportolok.Max(s => s.Eredmeny)} m");
-            Console.WriteLine($"Legrosszabb eredmény: {sportolok.Min(s => s.Eredmeny)} m");
-            Console.WriteLine($"Átlagos eredmény: {sportolok.Average(s => s.Eredmeny):F2} m");
-            Console.WriteLine($"Resztvevők száma: {sportolok.Count}");
-
-
-
-            List<double> rendezettEredmenyek = new List<double>();
-            for (int i = 0; i < sportolok.Count; i++)
-            {
-                rendezettEredmenyek.Add(sportolok[i].Eredmeny);
-            }
-            rendezettEredmenyek.Sort();
-
-            double median;
-            int n = rendezettEredmenyek.Count;
-            if (n % 2 == 0)
-                median = (rendezettEredmenyek[n / 2 - 1] + rendezettEredmenyek[n / 2]) / 2;
-            else
-                median = rendezettEredmenyek[n / 2];
-
-            Console.WriteLine($"Medián eredmény: {median} m");
-
-
-
-            Console.WriteLine("\nAdja meg az országkódot a szűréshez: ");
-            string keresetOrszag = Console.ReadLine().ToUpper();
-
-            List<Sportolo> szurtSportolok = new List<Sportolo>();
-            for (int i = 0; i < sportolok.Count; i++)
-            {
-                if (sportolok[i].OrszagKod == keresetOrszag)
+                //Kiszámitja a mediánt
+                List<double> rendezettEredmenyek = new List<double>(); //Eredmények listája rendezéshez
+                for (int i = 0; i < sportolok.Count; i++)
                 {
-                    szurtSportolok.Add(sportolok[i]);
+                    rendezettEredmenyek.Add(sportolok[i].Eredmeny); //Hozzáadja az eredményeket
                 }
+                rendezettEredmenyek.Sort(); //Eredmények növekvő sorrendbe rendezése
+
+                double median;
+                int n = rendezettEredmenyek.Count;
+                if (n % 2 == 0) //Páros szám esetén
+                    median = (rendezettEredmenyek[n / 2 - 1] + rendezettEredmenyek[n / 2]) / 2;
+                else //páratlan szám esetén
+                    median = rendezettEredmenyek[n / 2];
+
+                Console.WriteLine($"Medián eredmény: {median} m");
+
+
+                //Szürés országkód alapján
+                Console.WriteLine("\nAdja meg az országkódot a szűréshez: "); //Bekéri az országkódot a felhasználótól
+                string keresetOrszag = Console.ReadLine().ToUpper();
+
+                List<Sportolo> szurtSportolok = new List<Sportolo>();
+                for (int i = 0; i < sportolok.Count; i++)
+                {
+                    if (sportolok[i].OrszagKod == keresetOrszag) //Ha az országkód egyezik 
+                    {
+                        szurtSportolok.Add(sportolok[i]); //hozzáadaj a szürt listához
+                    }
+                }
+
+                Console.WriteLine("\nSzűrt eredmények: ");
+                for (int i = 0; i < szurtSportolok.Count; i++)
+                {
+                    Console.WriteLine(szurtSportolok[i]); //a kiszürt sportoló adatainak megjelénitése
+                }
+                //Szürt adatok mentése file-ban 
+                string fajl = "szurt_eredmenyek.txt";
+                List<string> szurtSportolokStr = new List<string>(); //File-ban mentendő adatok
+                for (int i = 0; i < szurtSportolok.Count; i++)
+                {
+                    szurtSportolokStr.Add(szurtSportolok[i].ToString()); // Sportoló adatait átalakitja szöveggé
+                }
+
+                File.WriteAllLines(fajl, szurtSportolokStr.ToArray()); //Beleirja a file-ba az adatokat
+
+                Console.WriteLine($"\nSzűrt adatok mentve a(z) {fajl} fájlba."); //Kiirja ha sikeresen adott adatok müködtek és mentve lett a file
+
+
+                Befejez();
+
             }
-
-            Console.WriteLine("\nSzűrt eredmények: ");
-            for (int i = 0; i < szurtSportolok.Count; i++)
-            {
-                Console.WriteLine(szurtSportolok[i]);
-            }
-
-            string fajl = "szurt_eredmenyek.txt";
-            List<string> szurtSportolokStr = new List<string>();
-            for (int i = 0; i < szurtSportolok.Count; i++)
-            {
-                szurtSportolokStr.Add(szurtSportolok[i].ToString());
-            }
-
-            File.WriteAllLines(fajl, szurtSportolokStr.ToArray());
-
-            Console.WriteLine($"\nSzűrt adatok mentve a(z) {fajl} fájlba.");
-
-
-            Befejez();
-
         }
     }
-}
+
--- Kalapacsvetes/Sportolo.cs
+++ Kalapacsvetes/Sportolo.cs
@@ -6,27 +6,28 @@ using System.Threading.Tasks;
 
 namespace Kalapacsvetes
 {
-    internal class Sportolo
+    internal class Sportolo //Sportoló class a versenyzök adatainak tárolása
     {
-        public int Helyezes { get; set; }
-        public double Eredmeny { get; set; }
-        public string Nev { get; set; }
-        public string OrszagKod { get; set; }
-        public string Helyszin { get; set; }
-        public DateTime Datum { get; set; }
+        public int Helyezes { get; set; } //Versenyző helyezése a versenyen
+        public double Eredmeny { get; set; } //Versenyző eredménye (dobott távolság)
+        public string Nev { get; set; } //Versenyző Neve
+        public string OrszagKod { get; set; } //Versenyző Ország Kódja
+        public string Helyszin { get; set; } //Verseny helyszine
+        public DateTime Datum { get; set; } //Verseny dátuma
 
         public Sportolo(int helyezes, double eredmeny, string nev, string orszagKod, string helyszin, DateTime datum)
         {
-            Helyezes = helyezes;
-            Eredmeny = eredmeny;
-            Nev = nev;
-            OrszagKod = orszagKod;
-            Helyszin = helyszin;
-            Datum = datum;
+            Helyezes = helyezes; //Beállitja a versenyzők helyezését
+            Eredmeny = eredmeny;//Beállitja a versenyzők eredményét
+            Nev = nev;//Beállitja a versenyző nevét
+            OrszagKod = orszagKod;//Beállitja az Országkódot
+            Helyszin = helyszin;//Beállitja a Verseny Helyszinét
+            Datum = datum;//Beállitja a Verseny Dátumát
         }
 
-        public override string ToString()
+        public override string ToString() 
         {
+            //Kiirat mindent egy sorban
             return $"{Helyezes}; {Eredmeny}m; {Nev}; {OrszagKod}; {Helyszin}; {Datum.ToShortDateString()}";
         }
 
